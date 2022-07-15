29f7e802dac00d78908fa4370df90fe1b5c6f573
Christian Moura <cdbm@cin.ufpe.br>
2022-07-15 00:05:12 -0300

    change minutes

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ setInterval(() => {
     }
 
     console.log(msg);
-    if (now.getHours() === 0 && now.getMinutes() === 10) {
+    if (now.getHours() === 0 && now.getMinutes() === 7) {
         tweet(msg);
     }
 
