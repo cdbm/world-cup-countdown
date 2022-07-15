b482552095b91267fe7f5555589fe98d9a3d0389
Christian Moura <cdbm@cin.ufpe.br>
2022-07-14 23:41:49 -0300

    bot v1

---
+++ index.js
@@ -0,0 +1,43 @@
+var Twitter = require('twitter');
+
+require('dotenv').config()
+
+const copa = new Date('November 21, 2022 03:24:00');
+
+var T = new Twitter({
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+tweet("Hello World!!");
+
+function tweet(text) {
+
+    T.post('statuses/update', { status: text }, function (error, tweet, response) {
+        if (error) throw error;
+        console.log("OK");
+    });
+}
+
+setInterval(() => {
+    let now = new Date();
+    let diff = copa.getTime() - now.getTime();
+    let TotalDays = Math.ceil(diff / (1000 * 3600 * 24));
+    let msg = ""
+
+    if (TotalDays > 1) {
+        msg = "Faltam " + TotalDays + " dias para a Copa do Mundo do Qatar";
+    } else if (TotalDays == 1) {
+        msg = "Falta " + TotalDays + " dia para a Copa do Mundo do Qatar";
+    } else if (TotalDays == 0) {
+        msg = "A Copa do Mundo do Qatar começa Hoje!!"
+    }
+
+    console.log(msg);
+    if (now.getHours() === 0 && now.getMinutes() === 1) {
+        tweet(msg);
+    }
+
+}, 60 * 1000)
